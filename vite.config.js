2d12c7d4b044c63c97c9f918f4b87edf18dada40
cel3ntato <adriano2024celentano@gmail.com>
2024-05-15 01:14:04 +0200

    structure

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        // input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/**/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
