dca9a0f29b8a9340354aef56a9a03dd80b751fef
cel3ntato <adriano2024celentano@gmail.com>
2024-05-19 00:43:12 +0200

    dom-task-6

--- src/js/dom-tasks-js/dom-task-js6.js
+++ src/js/dom-tasks-js/dom-task-js6.js
@@ -1,4 +1,33 @@
+import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
 //TODO: № 6 ==============================================
 // Додайде слухач кліку і визначте, коли клік відбувається
-// всередині елемента з id "place" і коли клік припадає поза зоною елемента
+// всередині елемента з id "place" і коли клік відбувається
+// поза зоною елемента
 //
+
+const box = document.getElementById('place');
+document.addEventListener('click', onClick);
+
+function onClick(event) {
+  const target = event.target;
+  if (target.id === 'place') {
+    iziToast.show({
+      message: '✅ Click inside the box',
+      color: 'green',
+      position: 'topRight',
+      progressBar: false,
+      timeout: 1000,
+      close: false,
+    });
+  } else {
+    iziToast.show({
+      message: '❌ Click outside the box',
+      color: 'red',
+      position: 'topRight',
+      progressBar: false,
+      timeout: 1000,
+      close: false,
+    });
+  }
+}
