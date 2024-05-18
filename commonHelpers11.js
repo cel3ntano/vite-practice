70aefb2e31d587955e666f5976f37b73966c75e4
cel3ntano <160436964+cel3ntano@users.noreply.github.com>
2024-05-18 22:43:34 +0000

    Deploying to gh-pages from @ cel3ntano/vite-practice@dca9a0f29b8a9340354aef56a9a03dd80b751fef 🚀

--- commonHelpers11.js
+++ commonHelpers11.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const n=document.querySelector("button"),o=document.querySelectorAll("p:not(.taskTitle)");n.addEventListener("click",c);function c(){o.forEach((t,e)=>{t.textContent=e+1})}
+import"./assets/styles-b793132a.js";const n=document.querySelector("button"),o=document.querySelectorAll("p:not(.taskTitle)");n.addEventListener("click",c);function c(){o.forEach((t,e)=>{t.textContent=e+1})}
 //# sourceMappingURL=commonHelpers11.js.map
