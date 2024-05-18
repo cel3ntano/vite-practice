97e70211954a54ba09ddeef01ec21e37adcfc8d3
cel3ntato <adriano2024celentano@gmail.com>
2024-05-18 23:36:32 +0200

    dom-task-5

--- src/js/dom-tasks-js/dom-task-js5.js
+++ src/js/dom-tasks-js/dom-task-js5.js
@@ -1,3 +1,24 @@
 //TODO: № 5 ==============================================
 // Кнопка "Зменшити" робить квадрат менше на 10 пікселів,
 // кнопка "Збільшити" - більше на 10 пікселів.
+
+const box = document.querySelector('#box');
+const btnDecrease = document.querySelector('#decrease');
+const btnIncrease = document.querySelector('#increase');
+
+btnDecrease.addEventListener('click', changeSize);
+btnIncrease.addEventListener('click', changeSize);
+let size = 10;
+
+function changeSize(event) {
+  const btn = event.target.id;
+  const width = box.offsetWidth;
+  const height = box.offsetHeight;
+  if (btn === 'increase') {
+    box.style.width = width + size + 'px';
+    box.style.height = height + size + 'px';
+  } else {
+    box.style.width = width - size + 'px';
+    box.style.height = height - size + 'px';
+  }
+}
