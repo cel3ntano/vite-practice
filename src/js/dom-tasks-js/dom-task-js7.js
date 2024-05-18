6bf5bc1c453c45db431aecd73db3f5482b8f0678
cel3ntato <adriano2024celentano@gmail.com>
2024-05-19 00:51:27 +0200

    dom-task-7

--- src/js/dom-tasks-js/dom-task-js7.js
+++ src/js/dom-tasks-js/dom-task-js7.js
@@ -1,3 +1,12 @@
 //TODO: № 7 ==============================================
 // Натиснувши кнопку "Подвоювати", збільшити значення
 // у кожному елементі списку у 2 рази
+
+const btn = document.getElementById('double');
+const list = document.querySelectorAll('.listItem');
+btn.addEventListener('click', onClick);
+function onClick() {
+  list.forEach(item => {
+    item.textContent *= 2;
+  });
+}
