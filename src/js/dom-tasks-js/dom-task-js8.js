26e5c68f5c0091a88c20106dadf1ae5375a5597c
cel3ntato <adriano2024celentano@gmail.com>
2024-05-19 01:42:42 +0200

    dom-task-8

--- src/js/dom-tasks-js/dom-task-js8.js
+++ src/js/dom-tasks-js/dom-task-js8.js
@@ -1,4 +1,25 @@
+import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
 //TODO: № 8 ==============================================
 // Вводимо число у input з id="test" - число
 // При кліку на кнопку, у інпут з id="result" повинен буди квадрат цього числа
 // Зробити перевірку на число, реалізувати за допомогою alert з текстом
+const input = document.getElementById('test');
+const result = document.getElementById('result');
+const btn = document.querySelector('.magic-button');
+
+btn.addEventListener('click', onClick);
+function onClick() {
+  if (isNaN(input.value)) {
+    iziToast.show({
+      message: '❌ Please, enter a number',
+      color: 'red',
+      position: 'topRight',
+      progressBar: false,
+      timeout: 2000,
+      close: false,
+    });
+  } else {
+    result.value = input.value ** 2;
+  }
+}
